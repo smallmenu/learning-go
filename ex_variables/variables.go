12b684e3c0599970a7bc604d96a6fdd3a1cceff2
niuchaoqun <smallmenu@163.com>
2021-12-16 14:40:24 +0800

    * update

---
+++ ex_variables/variables.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a = "inital"
+
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	// 声明未初始化，变量会初始化为：零值
+	var e int
+	fmt.Println(e)
+
+	// := 语法是声明并初始化变量的简写
+	f := "short"
+	fmt.Println(f)
+}
