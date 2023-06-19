ea2dc188a013d74f6d3adaa15ecd6219a06fa983
smallmenu <smallmenu@163.com>
2023-06-19 13:27:10 +0800

    update

--- ex_00_sort_by_function/sort_by_function.go
+++ ex_00_sort_by_function/sort_by_function.go
@@ -8,7 +8,7 @@ import (
 // 为了使用 Go 自定义函数排序，需要一个对应的类型，创建一个 []string 的别名，byLength
 type byLength []string
 
-// 实现 sort.Interface 接口的 Len、Less、和Swap 方法
+// Len 实现 sort.Interface 接口的 Len、Less、和Swap 方法
 func (s byLength) Len() int {
 	return len(s)
 }
--- ex_01_channel_close/channel_close.go
+++ ex_01_channel_close/channel_close.go
@@ -7,13 +7,14 @@ func main() {
 	done := make(chan bool)
 
 	// 模拟工作协程，使用 j, ok := <-jobs 接收数据，根据第二个值 ok， 如果 jobs 已经关闭，该值将是 false
-	// done 通道用于通知主线程
+
 	go func() {
 		for {
 			j, ok := <-jobs
 			if ok {
 				fmt.Println("received job", j)
 			} else {
+				// done 通道用于通知主线程
 				done <- true
 
 				// 这个打印应该放在后面，否则可能会打印多次，因为程序运行太快了
@@ -32,6 +33,7 @@ func main() {
 	// 关闭一个通道，意味着不能再向这个通道发送值，该特性可以向通道的接收方传达已经完成的信息
 	close(jobs)
 
-	// done 通道会阻塞到直到工作线程执行完毕，如果没有这个，程序可能就直接结束了，
+	// done 通道会阻塞到直到工作线程执行完毕，
+	// 如果没有这个，程序会一直收到 received all jobs
 	<-done
 }
